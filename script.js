4b3920ec19e5f0d4b548a128271fe39fbe907aca
Shubhanshu Mishra <shivamamethiup@gmail.com>
2026-01-09 18:05:09 +0530

    Remove URL opening from showMessage function
    Removed the window.open call that opens a URL.

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function showMessage() {
   alert("Thank you for viewing my project! 🚀");
-  window.open(
-    "https://shubhanshu-mishra85.github.io/web-development-project/",
-    "_blank"
-  );
 }
